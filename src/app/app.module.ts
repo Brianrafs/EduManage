db9f95cf9ea1a188e9081597aab5f6d19de3e049
Brianrafs <brianxdgmr@gmail.com>
2023-11-09 17:36:03 -0300

    Some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from 'src/layout/layout.module';
+import { LoginFormComponent } from 'src/layout/forms/login-form/login-form.component';
+import { NewUserFormComponent } from 'src/layout/forms/new-user-form/new-user-form.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { LayoutModule } from 'src/layout/layout.module';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        LoginFormComponent,
+        NewUserFormComponent,
         LayoutModule
     ],
   providers: [],
